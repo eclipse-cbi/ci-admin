b3d38043797528e3cc8dd4255b72aebf79c7e0d9
Frederic Gurr <frederic.gurr@eclipse-foundation.org>
2021-05-07 18:13:03 +0200

    Improve SonarCloud script
    Add usage, automatically add token to pass, set default SonarCloud
    organization (eclipse)

--- sonarcloud/create_sonarcloud_project.sh
+++ sonarcloud/create_sonarcloud_project.sh
@@ -17,12 +17,15 @@ set -o pipefail
 IFS=$'\n\t'
 
 SCRIPT_FOLDER="$(dirname "$(readlink -f "${0}")")"
+SCRIPT_NAME="$(basename "${0}")"
 
-NAME="${1:-}"
-PROJECT="${2:-}"
-ORGANIZATION="${3:-}"
+PROJECT_NAME="${1:-}"
+SONAR_NAME="${2:-}"
+SONAR_PROJECT="${3:-}"
+SONAR_ORG="${4:-eclipse}"
 
 SONAR_API_BASE_URL="https://sonarcloud.io/api"
+PW_STORE_PATH="cbi-pass/bots/${PROJECT_NAME}"
 
 if [[ ! -f "${SCRIPT_FOLDER}/../.localconfig" ]]; then
   echo "ERROR: File '$(readlink -f "${SCRIPT_FOLDER}/../.localconfig")' does not exists"
@@ -32,18 +35,35 @@ fi
 
 SONAR_TOKEN="$(cat "${SCRIPT_FOLDER}/../.localconfig" | jq -r '."sonar-token"')"
 
-if [ -z "${NAME}" ]; then
-  printf "ERROR: a name (e.g. 'Eclipse CBI Project' for CBI project) must be given.\n" >&2
+usage() {
+  printf "Usage: %s project_name sonar_name sonar_project_id [sonar_org]\n" "${SCRIPT_NAME}"
+  printf "\t%-16s project name (e.g. technology.cbi for CBI project).\n" "project_name"
+  printf "\t%-16s sonar_name (e.g. 'Eclipse CBI Project' for CBI project).\n" "sonar_name"
+  printf "\t%-16s sonar_project (e.g. 'org.eclipse.cbi').\n" "sonar_project"
+  printf "\t%-16s sonar_org (optional, default is 'eclipse').\n" "sonar_org"
+}
+
+if [ -z "${PROJECT_NAME}" ]; then
+  printf "ERROR: a project name (e.g. 'technology.cbi' for the CBI project) must be given.\n" >&2
+  usage
   exit 1
 fi
 
-if [ -z "${PROJECT}" ]; then
-  printf "ERROR: a project (e.g. 'org.eclipse_cbi') must be given.\n" >&2
+if [ -z "${SONAR_NAME}" ]; then
+  printf "ERROR: a name (e.g. 'Eclipse CBI Project' for the CBI project) must be given.\n" >&2
+  usage
   exit 1
 fi
 
-if [ -z "${ORGANIZATION}" ]; then
+if [ -z "${SONAR_PROJECT}" ]; then
+  printf "ERROR: a project (e.g. 'org.eclipse.cbi') must be given.\n" >&2
+  usage
+  exit 1
+fi
+
+if [ -z "${SONAR_ORG}" ]; then
   printf "ERROR: an organization (e.g. 'eclipse') must be given.\n" >&2
+  usage
   exit 1
 fi
 
@@ -77,7 +97,7 @@ create_project() {
 }
 
 create_token() {
-  local token_name="$1"
+  local token_name="Token for ${1:-}"
 
   echo "Creating SonarCloud token:"
   reply="$(curl_post "name=${token_name}" 'user_tokens/generate')"
@@ -87,10 +107,13 @@ create_token() {
   if echo "${reply}" | jq -e 'has("errors")' > /dev/null ; then
     echo "${reply}" | jq -r '.errors[].msg'
   else
-    echo "${reply}" | jq -r '.token'
+    token=$(echo "${reply}" | jq -r '.token')
+    echo "${token}"
+    # Add token to pass
+    echo "${token}" | pass insert --echo "${PW_STORE_PATH}/sonarcloud.io/token"
   fi
 }
 
-create_project "${NAME}" "${PROJECT}" "${ORGANIZATION}"
-create_token "Token for ${PROJECT}"
+create_project "${SONAR_NAME}" "${SONAR_PROJECT}" "${SONAR_ORG}"
+create_token "${SONAR_PROJECT}"
 

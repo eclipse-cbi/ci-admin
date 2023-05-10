e46b055ccd2e9cd687548ba4ac729692f26b6b34
Frederic Gurr <frederic.gurr@eclipse-foundation.org>
2023-05-10 17:37:22 +0200

    Improve rename_project.sh script

--- project/rename_project.sh
+++ project/rename_project.sh
@@ -42,8 +42,11 @@ if [[ -z "${NEW_PROJECT_NAME}" ]]; then
 fi
 
 echo
-echo "Old name: ${PROJECT_NAME}   =>   ${NEW_PROJECT_NAME}"
+echo "Old name: ${PROJECT_NAME}   =>   New name: ${NEW_PROJECT_NAME}"
 echo
+
+RENAME_LOG="rename_project_${PROJECT_NAME}_to_${NEW_PROJECT_NAME}.log"
+> "${RENAME_LOG}"
 
 # adapt pass credentials
 fix_pass() {
@@ -51,57 +54,83 @@ fix_pass() {
   password_store_dir="$("${CI_ADMIN_ROOT}/utils/local_config.sh" "get_var" "cbi-dir" "password-store")"
   local new_pass_project="bots/${NEW_PROJECT_NAME}"
 
+  echo "# Updating pass credentials..."
+
   # move pass credentials
   # check if it's already moved
   if [[ -d "${password_store_dir}/${new_pass_project}" ]]; then
-    echo "${password_store_dir}/${new_pass_project} already exists, skipping..."
+    echo " * [SKIP] New pass folder ${new_pass_project} already exists."
   else
     pushd "${password_store_dir}/bots/"
     git mv "${PROJECT_NAME}" "${NEW_PROJECT_NAME}"
     popd
+    echo " * [FIXED] pass folder was moved to ${new_pass_project}."
   fi
 
   if [[ "${OLD_SHORT_NAME}" != "${NEW_SHORT_NAME}" ]]; then
     # Gerrit
     if [[ -d "${password_store_dir}/${new_pass_project}/git.eclipse.org" ]]; then
-      echo "Updating Gerrit username (=> \"genie.${NEW_SHORT_NAME}\")..."
-      echo "genie.${NEW_SHORT_NAME}" | passw cbi insert --echo "${new_pass_project}/git.eclipse.org/username"
+      if [[ "$(passw cbi "${new_pass_project}/git.eclipse.org/username")" == "genie.${NEW_SHORT_NAME}" ]]; then
+        echo " * [SKIP] Gerrit username has been updated already."
+      else
+        echo "genie.${NEW_SHORT_NAME}" | passw cbi insert --echo "${new_pass_project}/git.eclipse.org/username" >> "${RENAME_LOG}"
+        echo " * [FIXED] Gerrit username updated (=> \"genie.${NEW_SHORT_NAME}\")."
+      fi
     fi
 
     # projects-storage.eclipse.org
     if [[ -d "${password_store_dir}/${new_pass_project}/projects-storage.eclipse.org" ]]; then
-      echo "Updating projects-storage username (=> \"genie.${NEW_SHORT_NAME}\")..."
-      echo "genie.${NEW_SHORT_NAME}" | passw cbi insert --echo "${new_pass_project}/projects-storage.eclipse.org/username"
+      if [[ "$(passw cbi "${new_pass_project}/projects-storage.eclipse.org/username")" == "genie.${NEW_SHORT_NAME}" ]]; then
+        echo " * [SKIP] projects-storage username has been updated already."
+      else
+        echo "genie.${NEW_SHORT_NAME}" | passw cbi insert --echo "${new_pass_project}/projects-storage.eclipse.org/username" >> "${RENAME_LOG}"
+        echo " * [FIXED] projects-storage username updated (=> \"genie.${NEW_SHORT_NAME}\")."
+      fi
     fi
 
     # GitHub
     if [[ -d "${password_store_dir}/${new_pass_project}/github.com" ]]; then
-      echo "Updating GitHub username and email (=> \"eclipse-${NEW_SHORT_NAME}-bot\", \"${NEW_SHORT_NAME}-bot@eclipse.org\")..."
-      echo "eclipse-${NEW_SHORT_NAME}-bot" | passw cbi insert --echo "${new_pass_project}/github.com/username"
-      echo "${NEW_SHORT_NAME}-bot@eclipse.org" | passw cbi insert --echo "${new_pass_project}/github.com/email"
+      local new_github_bot_name="eclipse-${NEW_SHORT_NAME}-bot"
+      local new_github_bot_email="${NEW_SHORT_NAME}-bot@eclipse.org"
+      if [[ "$(passw cbi "${new_pass_project}/github.com/username")" == "${new_github_bot_name}" ]] &&
+         [[ "$(passw cbi "${new_pass_project}/github.com/email")" == "${new_github_bot_email}" ]]; then
+        echo " * [SKIP] GitHub username and email have been updated already."
+      else
+        echo "${new_github_bot_name}" | passw cbi insert --echo "${new_pass_project}/github.com/username" >> "${RENAME_LOG}"
+        echo "${new_github_bot_email}" | passw cbi insert --echo "${new_pass_project}/github.com/email" >> "${RENAME_LOG}"
+        echo " * [FIXED] GitHub username and email updated (=> \"${new_github_bot_name}\", \"${new_github_bot_email}\")."
+      fi
     fi
 
     # GitLab
     if [[ -d "${password_store_dir}/${new_pass_project}/gitlab.eclipse.org" ]]; then
-      echo "Updating GitLab username and email (=> \"${NEW_SHORT_NAME}-bot\", \"${NEW_SHORT_NAME}-bot@eclipse.org\")..."
-      echo "${NEW_SHORT_NAME}-bot" | passw cbi insert --echo "${new_pass_project}/gitlab.eclipse.org/username"
-      echo "${NEW_SHORT_NAME}-bot@eclipse.org" | passw cbi insert --echo "${new_pass_project}/gitlab.eclipse.org/email"
+      local new_gitlab_bot_name="${NEW_SHORT_NAME}-bot"
+      local new_gitlab_bot_email="${NEW_SHORT_NAME}-bot@eclipse.org"
+      if [[ "$(passw cbi "${new_pass_project}/github.com/username")" == "${new_gitlab_bot_name}" ]] &&
+         [[ "$(passw cbi "${new_pass_project}/github.com/email")" == "${new_gitlab_bot_email}" ]]; then
+        echo " * [SKIP] GitLab username and email have been updated already."
+      else
+        echo "${new_gitlab_bot_name}" | passw cbi insert --echo "${new_pass_project}/gitlab.eclipse.org/username" >> "${RENAME_LOG}"
+        echo "${new_gitlab_bot_email}" | passw cbi insert --echo "${new_pass_project}/gitlab.eclipse.org/email" >> "${RENAME_LOG}"
+        echo " * [FIXED] GitLab username and email updated (=> \"${new_gitlab_bot_name}\", \"${new_gitlab_bot_email}\")."
+      fi
     fi
 
 #TODO more
 #TODO: create list of changes for external services like GitHub
 #TODO: automate renaming of accounts
-    echo "TODO: change accounts on websites (GitHub, GitLab, etc)"
-    read -rsp $'Once you are done, press any key to continue...\n' -n1
+    echo
+    echo " * [TODO] Change accounts on websites (GitHub, GitLab, etc)"
+    read -rsp $'   Once you are done, press any key to continue...\n' -n1
   else
-    echo "Project short name did not change, skipping renaming of usernames, etc in pass..."
+    echo " * [SKIP] Project short name did not change, skipping renaming of usernames, etc in pass..."
   fi
 }
 
 question() {
   local message="${1:-}"
   local action="${2:-}"
-  read -p "Do you want to ${message}? (Y)es, (N)o, E(x)it: " yn
+  read -rp "Do you want to ${message}? (Y)es, (N)o, E(x)it: " yn
   case $yn in
     [Yy]* ) ${action};;
     [Nn]* ) return ;;
@@ -116,35 +145,39 @@ rename_jipp() {
 }
 
 update_projects_bot_api() {
-  printf "\n# Update projects-bots-api...\n"
+  printf "\n# Updating projects-bots-api...\n"
 
-  echo "Connected to cluster?"
-  read -rp "Press enter to continue or CTRL-C to stop the script"
+#TODO: check automatically
+  echo "  Connected to cluster?"
+  read -rp "  Press enter to continue or CTRL-C to stop the script"
+  echo
+#TODO: check automatically
+  echo "  Pulled latest version of projects-bots-api?"
+  read -rp "  Press enter to continue or CTRL-C to stop the script"
 
-  echo "Pulled latest version of projects-bots-api?"
-  read -rp "Press enter to continue or CTRL-C to stop the script"
-
-  echo "Update project name in extensions.jsonnet file (if it exists)..."
   sed -i "s/${PROJECT_NAME}/${NEW_PROJECT_NAME}/" "${PROJECTS_BOTS_API_ROOT_FOLDER}/src/main/jsonnet/extensions.jsonnet"
+  echo "  * [FIXED] Updated project name in extensions.jsonnet file (if it exists)..."
 
-  "${PROJECTS_BOTS_API_ROOT_FOLDER}/regen_db.sh"
+  echo "  * Regenerating bot API DB..."
+  "${PROJECTS_BOTS_API_ROOT_FOLDER}/regen_db.sh" &>> "${RENAME_LOG}"
 
   printf "\n\n"
 #TODO: Show error if files are equal
-  read -rsp $'Once you are done with comparing the diff, press any key to continue...\n' -n1
-  "${PROJECTS_BOTS_API_ROOT_FOLDER}/deploy_db.sh"
+  read -rsp $'  Once you are done with comparing the diff, press any key to continue...\n' -n1
+  echo "  * Deploying bot API DB..."
+  "${PROJECTS_BOTS_API_ROOT_FOLDER}/deploy_db.sh" &>> "${RENAME_LOG}"
 
-  printf "\n# TODO: Double check that bot account has been added to API (https://api.eclipse.org/bots)...\n"
+  printf "\n * [TODO] Double check that bot account has been added to API (https://api.eclipse.org/bots)...\n"
   read -rsp $'Once you are done, press any key to continue...\n' -n1
 }
 
 fix_pass
+echo
 question "rename the JIPP" "rename_jipp"
-
-update_projects_bot_api
+question "update the projects bot API" "update_projects_bot_api"
 
 echo
-echo "TODO:"
-echo " * commit changes to cbi-pass"
-echo " * commit changes to JIRO repo"
+echo "# Manual steps:"
+echo " * [TODO] commit changes to cbi-pass"
+echo " * [TODO] commit changes to JIRO repo"
 

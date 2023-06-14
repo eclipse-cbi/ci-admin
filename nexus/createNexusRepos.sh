52ade8c5cc56c781670368c211ff8c6cbe2a96c9
Frederic Gurr <frederic.gurr@eclipse-foundation.org>
2023-06-14 11:15:05 +0200

    Clean up createNexusRepos.sh script

--- nexus/createNexusRepos.sh
+++ nexus/createNexusRepos.sh
@@ -8,6 +8,15 @@
 # SPDX-License-Identifier: EPL-2.0
 #*******************************************************************************
 
+# This script creates "releases" and "snapshots" repos plus a group on the Nexus server
+
+# Requirements:
+# * This script relies on credentials for Nexus being set in a .netrc file in your $HOME dir
+#   e.g.
+#      machine repo.eclipse.org
+#        login <username>
+#        password <password>
+
 #TODO: check if repo already exists
 
 # Bash strict-mode
@@ -17,13 +26,13 @@ set -o pipefail
 
 IFS=$'\n\t'
 
-script_name="$(basename ${0})"
+SCRIPT_NAME="$(basename "${BASH_SOURCE[0]}")"
 project_shortname="${1:-}"
 
 rest_api_base_url="http://repo.eclipse.org/service/local"
 
 usage() {
-  printf "Usage: %s project_shortname\n" "$script_name"
+  printf "Usage: %s project_shortname\n" "${SCRIPT_NAME}"
   printf "\t%-16s project short name (e.g. cbi for CBI project).\n" "project_name"
 }
 
@@ -41,60 +50,60 @@ if [[ "${project_shortname}" == *.* ]]; then
 fi
 
 nexus_curl() {
-    local json=$1
-    local rest_url=$2
-    local method=$3
-    
+    local json="${1:-}"
+    local rest_url="${2:-}"
+    local method="${3:-}"
+
     #debug -v -trace-ascii
-    if [[ ${method} == "GET" ]]; then
-        response=$(curl -L -s --netrc -H "Accept: application/json" -H "Content-Type: application/json" -X ${method} ${rest_url})
+    if [[ "${method}" == "GET" ]]; then
+        response=$(curl -L -s --netrc -H "Accept: application/json" -H "Content-Type: application/json" -X "${method}" "${rest_url}")
     else
-        response=$(curl -L -s --netrc -H "Accept: application/json" -H "Content-Type: application/json" -X ${method} -d "${json}" ${rest_url})
+        response=$(curl -L -s --netrc -H "Accept: application/json" -H "Content-Type: application/json" -X "${method}" -d "${json}" "${rest_url}")
     fi
-    
-    if [[ ${response} == *"errors"* ]]; then
-        error_msg=$(echo ${response} | jq -M '."errors"[0].msg') 
+
+    if [[ "${response}" == *"errors"* ]]; then
+        error_msg=$(echo "${response}" | jq -M '."errors"[0].msg') 
         echo "ERROR: ${error_msg}" >&2
         exit 1
     fi
-    
-    if [[ ${method} == "GET" ]]; then
+
+    if [[ "${method}" == "GET" ]]; then
         echo "${response}"
     fi
 }
 
 createRepo () {
-    local repo_id=$1
-    local repo_policy=$2
+    local repo_id="${1:-}"
+    local repo_policy="${2:-}"
     echo "Creating Nexus repo '${repo_id}'..."
     local json="{\"data\":{\"repoType\": \"hosted\", \"id\": \"${repo_id}\", \"name\": \"${repo_id}\", \"writePolicy\": \"ALLOW_WRITE_ONCE\", \"browseable\": true, \"indexable\": true, \"exposed\": true, \"notFoundCacheTTL\": 1440, \"repoPolicy\": \"${repo_policy}\", \"provider\": \"maven2\", \"providerRole\": \"org.sonatype.nexus.proxy.repository.Repository\", \"downloadRemoteIndexes\": false, \"checksumPolicy\": \"IGNORE\" }}"
-    nexus_curl "${json}" "${rest_api_base_url}/repositories" POST
+    nexus_curl "${json}" "${rest_api_base_url}/repositories" "POST"
 }
 
 createGroupRepo () {
-    local repo_id=$1
+    local repo_id="${1:-}"
     echo "Creating Nexus group repo '${repo_id}' with the following repos:"
     echo "* ${repo_id}-releases"
     echo "* ${repo_id}-snapshots"
     local json="{\"data\":{\"id\":\"${repo_id}\",\"name\":\"${repo_id}\",\"format\":\"maven2\",\"exposed\":true,\"provider\":\"maven2\",\"repositories\":[{\"id\":\"${repo_id}-releases\"},{\"id\":\"${repo_id}-snapshots\"}]}}"
-    nexus_curl "${json}" "${rest_api_base_url}/repo_groups" POST
+    nexus_curl "${json}" "${rest_api_base_url}/repo_groups" "POST"
 }
 
 addRepoToGroupRepo() {
-    local repo_id=$1
-    local group_id=$2
+    local repo_id="${1:-}"
+    local group_id="${2:-}"
     echo "Adding repo '${repo_id}' to group repo '${group_id}'..."
-    
+
     # get existing repos 
-    json_old=$(nexus_curl "" "${rest_api_base_url}/repo_groups/${group_id}" GET)
+    json_old=$(nexus_curl "" "${rest_api_base_url}/repo_groups/${group_id}" "GET")
     # add new repo
-    json=$(echo ${json_old} | jq ".data.repositories += [{\"id\": \"${repo_id}\", \"name\": \"${repo_id}\", \"resourceURI\": \"https://repo.eclipse.org/service/local/repo_groups/releases/${repo_id}\"}]")
+    json=$(echo "${json_old}" | jq ".data.repositories += [{\"id\": \"${repo_id}\", \"name\": \"${repo_id}\", \"resourceURI\": \"https://repo.eclipse.org/service/local/repo_groups/releases/${repo_id}\"}]")
     # update group
-    nexus_curl "${json}" "${rest_api_base_url}/repo_groups/${group_id}" PUT
+    nexus_curl "${json}" "${rest_api_base_url}/repo_groups/${group_id}" "PUT"
 }
 
 createBugReply () {
-    local repo_id=$1
+    local repo_id="${1:-}"
     cat << EOF
 Helpdesk response template:
 ------------------------
@@ -110,11 +119,11 @@ EOF
 }
 
 echo ""
-createRepo ${project_shortname}-releases RELEASE
-createRepo ${project_shortname}-snapshots SNAPSHOT
-createGroupRepo ${project_shortname}
-addRepoToGroupRepo ${project_shortname}-releases releases
-addRepoToGroupRepo ${project_shortname}-snapshots snapshots
-createBugReply ${project_shortname}
+createRepo "${project_shortname}-releases" "RELEASE"
+createRepo "${project_shortname}-snapshots" "SNAPSHOT"
+createGroupRepo "${project_shortname}"
+addRepoToGroupRepo "${project_shortname}-releases" "releases"
+addRepoToGroupRepo "${project_shortname}-snapshots" "snapshots"
+createBugReply "${project_shortname}"
 echo "Done."
 

b04b56b03b34c1bd49488a22fa9e33120e551ff8
Frederic Gurr <frederic.gurr@eclipse-foundation.org>
2023-07-24 19:43:06 +0200

    Improve and fix setup_jenkins_gitlab_integration.sh script
    * Fix YAML output
    * Only update projects-bots-api if necessary
    * Check if project has a Jenkins instance
    * Create webhook Jenkins credentials only if pass credentials were
    created
    * Add question if JCasC config should be created

--- gitlab/setup_jenkins_gitlab_integration.sh
+++ gitlab/setup_jenkins_gitlab_integration.sh
@@ -36,19 +36,23 @@ add_gitlab_jcasc_config() {
   printf "\n# Adding GitLab JCasC config to %s Jenkins instance...\n" "${PROJECT_NAME}"
   mkdir -p "${JIRO_ROOT_FOLDER}/instances/${PROJECT_NAME}/jenkins"
   JIRO_CONFIG_FILE="${JIRO_ROOT_FOLDER}/instances/${PROJECT_NAME}/jenkins/configuration.yml"
-
-  yq -i '.unclassified.gitLabConnectionConfig.connections.apiTokenId = "gitlab-api-token"' "${JIRO_CONFIG_FILE}"
-  yq -i '.unclassified.gitLabConnectionConfig.connections.clientBuilderId = "autodetect"' "${JIRO_CONFIG_FILE}"
-  yq -i '.unclassified.gitLabConnectionConfig.connections.connectionTimeout = 10' "${JIRO_CONFIG_FILE}"
-  yq -i '.unclassified.gitLabConnectionConfig.connections.ignoreCertificateErrors = false' "${JIRO_CONFIG_FILE}"
-  yq -i '.unclassified.gitLabConnectionConfig.connections.name = "gitlab.eclipse.org"' "${JIRO_CONFIG_FILE}"
-  yq -i '.unclassified.gitLabConnectionConfig.connections.readTimeout = 10' "${JIRO_CONFIG_FILE}"
-  yq -i '.unclassified.gitLabConnectionConfig.connections.url = "https://gitlab.eclipse.org"' "${JIRO_CONFIG_FILE}"
-
-  yq -i '.unclassified.gitLabServers.Servers.credentialsId = "gitlab-personal-access-token"' "${JIRO_CONFIG_FILE}"
-  yq -i '.unclassified.gitLabServers.Servers.name = "gitlab.eclipse.org"' "${JIRO_CONFIG_FILE}"
-  yq -i '.unclassified.gitLabServers.Servers.serverUrl = "https://gitlab.eclipse.org"' "${JIRO_CONFIG_FILE}"
-  yq -i '.unclassified.gitLabServers.Servers.webhookSecretCredentialsId = "gitlab-webhook-secret"' "${JIRO_CONFIG_FILE}"
+  if [[ ! -f "${JIRO_CONFIG_FILE}" ]]; then
+    touch "${JIRO_CONFIG_FILE}"
+  fi
+  output='.unclassified += {"gitLabConnectionConfig":{"connections":[{'
+  output+='"apiTokenId": "gitlab-api-token",'
+  output+='"clientBuilderId" : "autodetect",'
+  output+='"connectionTimeout": 10,'
+  output+='"ignoreCertificateErrors": false,'
+  output+='"name": "gitlab.eclipse.org",'
+  output+='"readTimeout": 10,'
+  output+='"url": "https://gitlab.eclipse.org"}]},'
+  output+='"gitLabServers": { "Servers": [ {'
+  output+='"credentialsId": "gitlab-personal-access-token",'
+  output+='"name": "gitlab.eclipse.org",'
+  output+='"serverUrl": "https://gitlab.eclipse.org",'
+  output+='"webhookSecretCredentialsId": "gitlab-webhook-secret"}]}}'
+  yq -i "${output}" "${JIRO_CONFIG_FILE}"
 
   printf "\n# Reloading configuration of the Jenkins instance...\n"
 
@@ -62,8 +66,13 @@ add_gitlab_jcasc_config() {
 }
 
 update_projects_bot_api() {
-  printf "\n# Updating projects-bots-api...\n"
-  "${PROJECTS_BOTS_API_ROOT_FOLDER}"/regen_db.sh
+  # check if project bot api entry already exists:
+  if ! curl -sSL "https://api.eclipse.org/bots" | jq -e '.[] | select(.projectId=="'${PROJECT_NAME}'") | has("gitlab.eclipse.org")' > /dev/null ; then
+    printf "\n# Updating projects-bots-api...\n"
+    "${PROJECTS_BOTS_API_ROOT_FOLDER}"/regen_db.sh
+  else
+    printf "\n# projects-bots-api entry for gitlab.eclipse.org already exists. Skipping...\n"
+  fi
 }
 
 add_bot_to_group() {
@@ -80,6 +89,7 @@ create_group_webhook() {
   group_name="${SHORT_NAME}" # this only works for the default group
   printf "\n# Creating group webhook for group '${group_name}'...\n"
   "${SCRIPT_FOLDER}/create_gitlab_webhook.sh" "group" "${PROJECT_NAME}" "${group_name}"
+  "${JIRO_ROOT_FOLDER}/jenkins-create-credentials-token.sh" "gitlab_webhook_secret" "${PROJECT_NAME}"
 }
 
 instructions_template() {
@@ -89,7 +99,7 @@ instructions_template() {
 Post the following on the corresponding HelpDesk issue:
 -------------------------------------------------------
 
-A GitLab bot user for ${PROJECT_NAME} has been created along with credentials that have been added to the CI instance.
+A GitLab bot user for ${PROJECT_NAME} has been created, along with credentials that have been added to the CI instance (SSH credentials ID: `gitlab-bot-ssh`).
 A webhook that will trigger CI jobs has been added as well.
 
 The recommended way of creating a GitLab triggered job and handle merge request is as follows:
@@ -104,24 +114,32 @@ EOF
   read -rsp $'Once you are done, press any key to continue...\n' -n1
 }
 
-####
+add_jenkins_credentials() {
+  printf "\n# Adding GitLab bot credentials to Jenkins instance...\n"
+  "${JIRO_ROOT_FOLDER}/jenkins-create-credentials-token.sh" "gitlab" "${PROJECT_NAME}"
+  "${JIRO_ROOT_FOLDER}/jenkins-create-credentials-token.sh" "gitlab_pat" "${PROJECT_NAME}"
+  "${JIRO_ROOT_FOLDER}/jenkins-create-credentials.sh" "${PROJECT_NAME}"
+}
+
+#### MAIN
 
 echo "# Creating a GitLab bot user..."
 "${SCRIPT_FOLDER}/create_gitlab_bot_user.sh" "${PROJECT_NAME}"
 
-update_projects_bot_api
-
 add_bot_to_group
 
-printf "\n# Adding GitLab bot credentials to Jenkins instance...\n"
-"${JIRO_ROOT_FOLDER}/jenkins-create-credentials-token.sh" "gitlab" "${PROJECT_NAME}"
-"${JIRO_ROOT_FOLDER}/jenkins-create-credentials-token.sh" "gitlab_pat" "${PROJECT_NAME}"
-"${JIRO_ROOT_FOLDER}/jenkins-create-credentials-token.sh" "gitlab_webhook_secret" "${PROJECT_NAME}"
-"${JIRO_ROOT_FOLDER}/jenkins-create-credentials.sh" "${PROJECT_NAME}"
+update_projects_bot_api
 
-add_gitlab_jcasc_config
+#check if project has a Jenkins instance
+if [[ -d "${JIRO_ROOT_FOLDER}/instances/${PROJECT_NAME}" ]]; then
+  printf "\nFound Jenkins instance for %s...\n" "${PROJECT_NAME}"
+  _question_action "add Jenkins credentials" add_jenkins_credentials
+  echo
+  _question_action "create a group webhook" create_group_webhook
+  echo
+  _question_action "add GitLab JCasC config" add_gitlab_jcasc_config
+  instructions_template
+fi
 
-_question_action "create a group webhook" create_group_webhook
-
-instructions_template
-
+printf "\n\n# TODO: Commit changes to pass...\n"
+read -rsp $'Once you are done, press any key to continue...\n' -n1

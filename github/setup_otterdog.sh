c164a2d8c9d31ea497ada4e5968106806b0f67df
Frederic Gurr <frederic.gurr@eclipse-foundation.org>
2024-04-29 20:50:49 +0200

    Fix copyright year

--- github/setup_otterdog.sh
+++ github/setup_otterdog.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #*******************************************************************************
-# Copyright (c) 2021 Eclipse Foundation and others.
+# Copyright (c) 2024 Eclipse Foundation and others.
 # This program and the accompanying materials are made available
 # under the terms of the Eclipse Public License 2.0
 # which is available at http://www.eclipse.org/legal/epl-v20.html,

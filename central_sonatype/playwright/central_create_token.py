30f464f5604a0750176dcfe1d5e9d906c3a83209
Frederic Gurr <frederic.gurr@eclipse-foundation.org>
2025-11-17 14:17:23 +0100

    Fix central token generation after UI workflow changes

--- central_sonatype/playwright/central_create_token.py
+++ central_sonatype/playwright/central_create_token.py
@@ -1,27 +1,43 @@
 import sys
 import common
+import pyperclip
 from playwright.sync_api import sync_playwright, expect
 
 
 def setup_token(page, project_name):
     common.nav_to_token_settings(page)
 
-    page.locator('[data-test="name-code"]').click()
+    token_name = "API-Token"
 
-    token_username = page.locator('[data-test="name-code"]').input_value()
-    token_pass = page.locator('[data-test="pass-code"]').input_value()
-    
+    # deal with existing token
+    page.get_by_role("heading", name="Active Tokens", exact=True).click()
+    if page.get_by_role("heading", name=token_name, exact=True).is_visible():
+        print("Token '" + token_name + "' already exists. Skipping...")
+        return
+
     print("Register token")
+    page.locator("[data-test=\"refresh-btn\"]").click()
+    page.get_by_label("Token Name").fill(token_name)
+
+    page.get_by_text("Does not expire", exact=True).click()
+    page.locator("[data-test=\"add-token-submit\"]").click()
+
+    page.get_by_label("Copy Username", exact=True).click()
+    token_username = pyperclip.paste()
+    page.get_by_label("Copy Password").click()
+    token_password = pyperclip.paste()
+
     print("Central token name: " + token_username)
-    if token_username == "" or token_pass == "":
+    print("Central token pw: " + token_password)
+    if token_username == "" or token_password == "":
         print("ERROR: central tokens are empty")
         sys.exit(1)
 
     # add token to pass
     common.add_to_pass(project_name, token_username, "token-username")
     common.add_to_pass(project_name, token_pass, "token-password")
-    
-    page.locator('[data-test="close-view-token-modal"]').click()
+
+    page.locator("[data-test=\"close-view-token-modal\"]").click()
 
 
 def main():

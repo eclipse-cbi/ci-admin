0a40258a8b7b81f88da67791ee67d55332fcbf14
sebastien.heurtematte <sebastien.heurtematte@eclipse-foundation.org>
2026-04-27 16:29:41 +0200

    feat: enhance otterdog integration
    Signed-off-by: sebastien.heurtematte <sebastien.heurtematte@eclipse-foundation.org>

--- github/setup_otterdog.sh
+++ github/setup_otterdog.sh
@@ -10,13 +10,18 @@
 
 # Bash strict-mode
 set -o errexit
-set -o nounset
+# set -o nounset
 set -o pipefail
 
 IFS=$'\n\t'
 SCRIPT_FOLDER="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 CI_ADMIN_ROOT="${SCRIPT_FOLDER}/.."
-OTTERDOG_CONFIGS_ROOT="$("${CI_ADMIN_ROOT}/utils/local_config.sh" "get_var" "otterdog-configs-root-dir")"
+
+if [[ -z "${OTTERDOG_CONFIG_ROOT}" ]]; then
+  OTTERDOG_CONFIG_ROOT="$("${CI_ADMIN_ROOT}/utils/local_config.sh" "get_var" "otterdog-configs-root-dir")"
+fi
+
+echo "OTTERDOG_CONFIG_ROOT: ${OTTERDOG_CONFIG_ROOT}"
 
 #shellcheck disable=SC1091
 source "${SCRIPT_FOLDER}/../utils/common.sh"
@@ -34,13 +39,8 @@ GH_ORG="${2:-eclipse-${SHORT_NAME}}"
 
 echo "GH_ORG: ${GH_ORG}"
 
-check_if_bot_exists() {
-  printf "\n# Check if bot exists...\n"
-  if _check_pw_does_not_exist "${PROJECT_NAME}" "github.com/username"; then
-    #FIXME use setup_github_bot.sh script
-    printf "\n# TODO: Create bot account (semi manually)...\n"
-    read -rsp $'Once you are done, press any key to continue...\n' -n1
-  fi
+setup_gh_bot() {
+  "${SCRIPT_FOLDER}/setup_github_bot.sh" "${PROJECT_NAME}"
 }
 
 make_bot_owner() {
@@ -56,35 +56,75 @@ create_otterdog_token() {
   fi
 }
 
+test_weblogin() {
+  printf "\n# Testing otterdog web-login...\n"
+  otterdog web-login "${GH_ORG}"
+  echo "Waiting for 30s until new 2FA token is ready..."
+  sleep 30
+  read -rsp $'Once you are done, press any key to continue...\n' -n1
+}
+
 prepare_otterdog() {
   #TODO: check if otterdog is installed
   printf "\n# Prepare Otterdog...\n"
-  pushd "${OTTERDOG_CONFIGS_ROOT}" > /dev/null
-  # Add config to otterdog.json
+  pushd "${OTTERDOG_CONFIG_ROOT}" > /dev/null
+  echo "Updating otterdog config repo..."
+  
+  # Check for unstaged changes and stash if necessary
+  if ! git diff-files --quiet; then
+    echo "Unstaged changes detected. Stashing them..."
+    git stash push -u -m "Auto-stash before setup_otterdog pull"
+    STASHED=true
+  else
+    STASHED=false
+  fi
+  
+  git pull
+  
+  # Reapply stashed changes if any
+  if [[ "${STASHED}" == "true" ]]; then
+    echo "Reapplying stashed changes..."
+    git stash pop
+  fi
+  
+  echo "Adding config to otterdog.json..."
   cat <<EOF
     {
       "name": "${PROJECT_NAME}",
       "github_id": "${GH_ORG}"
     },
+
+    file://${OTTERDOG_CONFIG_ROOT}/otterdog.json
 EOF
-  printf "\n# TODO: Add config to ottedog.json manually...\n"
+  printf "\n# TODO: Add config to otterdog.json manually...\n"
   read -rsp $'Once you are done, press any key to continue...\n' -n1
-  # Test login with otterdog webui
-  otterdog web-login "${GH_ORG}"
+  
+  # Ask if user wants to test web-login
+  _question_action "test otterdog web-login" test_weblogin
+  
+  # Show the changes to be committed
+  printf "\n# Changes to otterdog.json:\n"
+  git --no-pager diff otterdog.json
+  
   # Commit and push otterdog.json change
-  printf "\n# TODO: Commit and push otterdog.json change manually...\n"
-  echo "Waiting for 30s until new 2FA token is ready..."
-  sleep 30
-  read -rsp $'Once you are done, press any key to continue...\n' -n1
+  printf "\n# Committing and pushing otterdog.json changes...\n"
+  read -rp "Enter the issue URL (e.g., https://gitlab.eclipse.org/eclipsefdn/emo-team/emo/-/issues/1106): " ISSUE_URL
+  git add otterdog.json
+  git commit -m "feat: add project ${PROJECT_NAME}
+
+Related to ${ISSUE_URL}"
+  git push
+
   popd > /dev/null
 }
 
 otterdog_config() {
   printf "\n# Configure Otterdog...\n"
-  #TODO: check if next commands can be skipped
-  pushd "${OTTERDOG_CONFIGS_ROOT}" > /dev/null
+  pushd "${OTTERDOG_CONFIG_ROOT}" > /dev/null
   # Import existing resources
   otterdog import "${GH_ORG}" || true
+  printf "\n# TODO: Cleanup manually...\n"
+  read -rsp $'Once you are done, press any key to continue...\n' -n1
   # Create default resources
   otterdog apply "${GH_ORG}" || true
   printf "\n# TODO: Check possible issues and cleanup manually...\n"
@@ -95,12 +135,16 @@ otterdog_config() {
 }
 
 install_otterdog_gh_app() {
-  local otterdog_app_url="https://github.com/organizations/EclipseFdn/settings/apps/eclipse-otterdog/installations"
-  printf "\n# Install Otterdog app...\n"
-  echo "* Go to ${otterdog_app_url} (should open browser automatically)"
-  echo "* Select GitHub org and 'Install'"
-  _open_url "${otterdog_app_url}"
-  read -rsp $'Once you are done, press any key to continue...\n' -n1
+  # local otterdog_app_url="https://github.com/organizations/EclipseFdn/settings/apps/eclipse-otterdog/installations"
+  # printf "\n# Install Otterdog app...\n"
+  # echo "* Go to ${otterdog_app_url} (should open browser automatically)"
+  # echo "* Select GitHub org and 'Install'"
+  # _open_url "${otterdog_app_url}"
+  # read -rsp $'Once you are done, press any key to continue...\n' -n1
+  echo "* Installing GitHub app via CLI..."
+  otterdog install-app -a eclipse-eca-validation ${GH_ORG}
+  otterdog install-app -a eclipse-otterdog ${GH_ORG}
+  otterdog install-app -a eclipse-foundation-sync ${GH_ORG}
 }
 
 issue_comment() {
@@ -122,7 +166,8 @@ A workflow will automatically run and show you the changes that will be applied 
 The documentation of all supported settings can be found here: https://otterdog.readthedocs.io/en/latest/reference/resource-format/
 
 A list of all Eclipse projects that have it also enabled can be accessed here: https://eclipsefdn.github.io/otterdog-configs/
-That is often quite helpful to get ideas about how others do their configurations.
+
+You can also navigate to https://otterdog.eclipse.org/projects/${PROJECT_NAME} to get more insights for your project in a dashboard like view.
 
 If you have any problems, let us know.
 EOF
@@ -131,24 +176,22 @@ EOF
 #### MAIN
 
 # Check if bot user exists, if not, create
-check_if_bot_exists
+_question_action "Setup GitHub bot" setup_gh_bot
 
 # Make bot user the owner of the GH org
 make_bot_owner
 
 # Create Otterdog PAT token for bot user
-create_otterdog_token
+_question_action "Create otterdog token" create_otterdog_token
 
 # Prepare otterdog
-prepare_otterdog
+_question_action "configure otterdog configuration file: otterdog.json" prepare_otterdog
 
 # Install Eclipse Otterdog GH app,
-# required before setting up otterdog
-# otherwise some ruleset might not be applied correctly
-install_otterdog_gh_app
+_question_action "install GitHub apps" install_otterdog_gh_app
 
 # Set up otterdog
-otterdog_config
+_question_action "configure otterdog GitHub Organization"  otterdog_config
 
 # Comment on the ticket
 issue_comment

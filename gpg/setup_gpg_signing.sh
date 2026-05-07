5ac30ec0fe2f5af9975413973add0bc93b6c2ddc
sebastien.heurtematte <sebastien.heurtematte@eclipse-foundation.org>
2026-05-07 15:28:59 +0200

    refactor: remove ossrh reference with central portal
    Signed-off-by: sebastien.heurtematte <sebastien.heurtematte@eclipse-foundation.org>

--- gpg/setup_gpg_signing.sh
+++ gpg/setup_gpg_signing.sh
@@ -8,7 +8,7 @@
 # SPDX-License-Identifier: EPL-2.0
 #*******************************************************************************
 
-# Setup GPG signing only (not for OSSRH/Maven Central)
+# Setup GPG signing only (not for central portal/Maven Central)
 
 # Bash strict-mode
 set -o errexit
